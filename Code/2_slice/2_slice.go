9a4c94978a24395713899b92ae8d5998ffafbd65
lzw <657434763@qq.com>
2022-01-06 14:51:52 +0800

    add

--- Code/2_slice/2_slice.go
+++ Code/2_slice/2_slice.go
@@ -5,17 +5,17 @@ import (
 )
 
 func main() {
-	s := []int{1, 2, 3,3,3,3}
-
-	for i,num := range s{
-		if num == 3 {
-			s = append(s[:i],s[i+1:]...)
-			i--
-
-		}
-	}
-
-	fmt.Println(s)
+	//s := []int{1, 2, 3,3,3,3}
+	//
+	//for i,num := range s{
+	//	if num == 3 {
+	//		s = append(s[:i],s[i+1:]...)
+	//		i--
+	//
+	//	}
+	//}
+	//
+	//fmt.Println(s)
 	//// 7为大小 9位最大容量
 	//var ss = make([]int, 7, 9)
 	//printSlice(s)
@@ -34,6 +34,15 @@ func main() {
 	//
 	///* 默认下限为 0*/
 	//fmt.Println("numbers[:3] ==", numbers[:3])
+
+	array := [10]int{1,2,3,4,5,6,7,8,9,10}
+
+	var slice = array[0:10]
+
+	fmt.Println("lenth of slice: ", len(slice))
+	fmt.Println("capacity of slice: ", cap(slice))
+	fmt.Println(slice)
+	fmt.Println(&slice[0] == &array[5])
 }
 
 func printSlice(x []int) {
