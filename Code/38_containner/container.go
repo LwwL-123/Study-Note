6b6b5d1a43a5f9e776cc3d9845887116cec57095
LwwL-123 <657434763@qq.com>
2022-03-10 12:12:37 +0800

    docs(*):新增 链表

--- Code/38_containner/container.go
+++ Code/38_containner/container.go
@@ -7,9 +7,75 @@ import (
 )
 
 func main() {
-	ring1()
-}
 
+	l1 := list.New()
+	l2 := list.New()
+	var num1,num2 int
+	fmt.Scan(&num1)
+
+	for i:=0;i<num1;i++{
+		var tmp int
+		fmt.Scan(&tmp)
+		l1.PushBack(tmp)
+	}
+
+	fmt.Scan(&num2)
+	for i:=0;i<num2;i++{
+		var tmp int
+		fmt.Scan(&tmp)
+		l2.PushBack(tmp)
+	}
+
+
+	l3 := list.New()
+	res := l3.Back()
+
+	e1 := l1.Back()
+	e2 := l2.Back()
+	for e1 != nil && e2 != nil {
+		val := e1.Value.(int) + e2.Value.(int)
+
+		if val / 10 > 0 {
+			if res != nil {
+				res.Value = res.Value.(int) + (val % 10)
+			}else {
+				l3.PushFront(val % 10)
+			}
+
+			l3.PushFront(1)
+			res = l3.Front()
+		}else {
+			if res != nil {
+				res.Value = res.Value.(int) + (val % 10)
+				res = l3.Front().Prev()
+			}else {
+				l3.PushFront(val)
+				res = l3.Front().Prev()
+			}
+		}
+
+		e1 = e1.Prev()
+		e2 = e2.Prev()
+	}
+
+	for e1 != nil {
+		l3.PushFront(e1.Value)
+		e1 = e1.Prev()
+	}
+
+	for e2 != nil {
+		l2.PushFront(e2.Value)
+		e2 = e2.Prev()
+	}
+
+	print(l3)
+
+}
+func print(l1 *list.List) {
+	for l := l1.Front();l!=nil;l=l.Next() {
+		fmt.Printf("%d ", l.Value)
+	}
+}
 func list1() {
 	// Create a new list and put some numbers in it.
 	l := list.New()
@@ -37,13 +103,9 @@ func ring1() {
 		a = a.Next()
 	}
 
-	a.
-
 
 	a.Do(func(i interface{}) {
 		fmt.Println(i.(string))
 	})
-
-
 
 }
--- Code/test/test.go
+++ Code/test/test.go
@@ -1,8 +1,45 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+
+	a := trans("This is a sample",16)
+	fmt.Println(a)
+	//l := list.New()
+	//for i:=1;i<=6;i++ {
+	//	l.PushBack(i)
+	//}
+	//
+	//l2 := list.New()
+	//
+	//cur := l.Front()
+	//for cur != nil {
+	//	tmp := cur.Next()
+	//	l2.PushBack(tmp.Value)
+	//
+	//	l.Remove(cur.Next())
+	//	cur = cur.Next()
+	//}
+	//
+	//l.PushBackList(l2)
+	//
+	//
+	//for e:=l.Front();e!=nil;e=e.Next(){
+	//	fmt.Println(e.Value)
+	//}
+
+
+
+
+
+
+
+
+
 	//cpunum := flag.Int("cpunum", 0, "cpunum")
 	//flag.Parse()
 	//fmt.Println("使用的cpunum:", *cpunum)
@@ -47,8 +84,8 @@ func main() {
 	//}
 	//c := b.p()
 	//fmt.Println(c)
-	a := 1
-	reflect.TypeOf(a)
+	//a := 1
+	//reflect.TypeOf(a)
 
 
 
@@ -58,7 +95,39 @@ type T interface {
 	p()
 }
 
+func trans( s string ,  n int ) string {
+	stack := make([]string,n)
+	num := 0
 
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' '{
+			num++
+		}else {
+			if s[i] >= 'a' && s[i] <= 'z'{
+				stack[num] = fmt.Sprintf("%s%s",stack[num],strings.ToUpper(string(s[i])))
+			}else {
+				stack[num] = fmt.Sprintf("%s%s",stack[num],strings.ToLower(string(s[i])))
+			}
+
+
+
+		}
+
+	}
+	stack = stack[:num+1]
+	reverse(&stack)
+	res := strings.Join(stack," ")
+
+	return res
+}
+
+func reverse(s *[]string){
+	length := len(*s)
+	for i:=0;i < length /2; i++ {
+		(*s)[i],(*s)[length-i-1] = (*s)[length-i-1],(*s)[i]
+	}
+
+}
 //type test
 
 //package main
